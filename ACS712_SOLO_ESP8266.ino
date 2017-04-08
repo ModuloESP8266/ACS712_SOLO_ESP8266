4a4bea1e3b80fe703c45e852f6f466d18af8bfdf
Diego Giovanazzi <diegogiovanazzi@gmail.com>
2017-04-08 10:11:44 -0300

    add
    se agregaron partes funcionales dle acs712 en el esp8266

--- ACS712_SOLO_ESP8266.ino
+++ ACS712_SOLO_ESP8266.ino
@@ -1,4 +1,19 @@
 
+
+/*
+Measuring AC Current Using ACS712
+
+const int sensorIn = A0;
+float  mVperAmp = 37.0;// 185; // use 100 for 20A Module and 66 for 30A Module
+float Voltage = 0;
+float VRMS = 0;
+float AmpsRMS = 0;
+float AmpFinalRMS=0;
+float PowRMS=0;
+////////////////////////////////
+*/
+
+
 const int Relay_2=5;
 /*
 Measuring AC Current Using ACS712
@@ -135,3 +150,56 @@ float getVPP(){
     Serial.print(" minValue:");Serial.println(minValue);
    return result;
  }
+
+
+
+ /*
+void Consumo_ACS712() {
+ 
+  float ajuste=0.0;//-.08;
+  float Voltaje;
+
+  digitalWrite(Led_Verde,false);
+  AmpsRMS=(TrueRMSMuestras()*1000)/mVperAmp;///0.037;
+  AmpFinalRMS=AmpsRMS+ajuste;
+  
+  if(AmpFinalRMS<0.040){// 1 muestra 26.1 mA =>mVperAmp 37mv
+    AmpFinalRMS=0;
+   
+    }
+  
+  PowRMS=220.0*AmpFinalRMS;
+   
+  PowRMS_str=String(PowRMS);
+  AmpsRMS_str=String(AmpFinalRMS);
+  Serial.print("AmpFinalRMS:");Serial.println(AmpFinalRMS);
+  Serial.print("PowRMS:");Serial.println(PowRMS);
+  digitalWrite(Led_Verde,true);
+}
+
+float TrueRMSMuestras(){
+  
+  float result=0,conv=0,Acumulador=0,suma=0;
+  int readValue;             //value read from the sensor
+  int Count=0;
+  uint32_t start_time = millis();
+  // while((millis()-start_time )< 200){ 
+  while(Count < 400) 
+   {   
+       delayMicroseconds(960); 
+       //delayMicroseconds(500); 
+       Count++;
+       readValue = analogRead(A0);
+       conv=(((readValue-511)*1.0)/1024.0);
+       Acumulador=Acumulador+sq(conv);
+      
+   }
+     suma=Acumulador/Count;
+     result=sqrt(suma);
+     return result;
+  }
+
+*/
+
+
+ 
